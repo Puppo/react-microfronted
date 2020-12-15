738eeff3f9617f01229900a336ae49642e986134
Luca Del Puppo <luca@delpuppo.net>
2020-12-15 23:24:28 +0100

    feat: integrate auth and lazy load

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -1,25 +1,27 @@
 const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 
 const { dependencies } = require("../package.json");
 
-const domain = process.env.PRODUCTION_DOMAIN;
-
 const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/container/latest/",
+    publicPath: "/auth/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "container",
-      remotes: {
-        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-        auth: `auth@${domain}/remoteEntry.js`,
+      name: "auth",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./AuthApp": "./src/bootstrap",
       },
       shared: dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
     }),
   ],
 };
--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -28,7 +28,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 };
 
 if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#_marketing_dev_root");
+  const devRoot = document.querySelector("#_auth_dev_root");
   if (devRoot) {
     mount(devRoot, {
       defaultHistory: createBrowserHistory(),
